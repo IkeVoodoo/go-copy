25d8a3f5c0b1007c893804cb319fdb0aa8af702b
IkeVoodoo <ikevoodoo@refinedtech.dev>
2025-02-05 23:34:33 +0100

    Begin command setup

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/integrii/flaggy"
+)
+
+func validateFilePath(path string, shouldExist bool) (os.FileInfo, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if !shouldExist {
+				return nil, nil
+			}
+
+			return nil, fmt.Errorf("path does not exist: %s", path)
+		}
+
+		return nil, fmt.Errorf("unable to access path: %s", path)
+	}
+
+	return info, nil
+}
+
+func exitOnError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	flaggy.SetName("copy")
+	flaggy.SetName("Simple and intuitive CLI copy alternative")
+	flaggy.SetVersion("1.0.0")
+
+	var sourcePath string
+	flaggy.AddPositionalValue(&sourcePath, "source_path", 1, true, "The source file or directory to copy from")
+
+	var destPath string
+	flaggy.AddPositionalValue(&destPath, "dest_path", 2, true, "The destination file or directory to copy to")
+
+	flaggy.Parse()
+
+	sourceInfo, err := validateFilePath(sourcePath, true)
+	exitOnError(err)
+
+	destInfo, err := validateFilePath(destPath, false)
+	exitOnError(err)
+
+	fmt.Println(sourceInfo.Name())
+	if destInfo == nil {
+		fmt.Println(destPath)
+	} else {
+		fmt.Println(destInfo.Name())
+	}
 }
